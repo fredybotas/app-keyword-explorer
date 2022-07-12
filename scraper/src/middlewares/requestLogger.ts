68ee67b1a391d17c9f490e05b4f8b4e204103991
Michal Manak <fredybotas@gmail.com>
2022-07-12 22:28:38 +0200

    Change http request logger name

--- scraper/src/middlewares/requestLogger.ts
+++ scraper/src/middlewares/requestLogger.ts
@@ -3,7 +3,7 @@ import { getLogger, LogLevel } from '../utils/logger';
 import log4js from 'log4js';
 
 export function middleware(): RequestHandler {
-  return log4js.connectLogger(getLogger(), {
+  return log4js.connectLogger(getLogger('HTTP'), {
     level: LogLevel.INFO,
     statusRules: [{ from: 500, to: 599, level: LogLevel.ERROR }],
   });
