6c5f364b57ede4e17d561c5cbdbbbde99e7ddc21
Michal Manak <fredybotas@gmail.com>
2022-07-31 19:58:23 +0200

    scraper-api: Add RateLimitingStoreProxy logic for search functionality

--- scraper/src/stores/RateLimitingProxyStore.ts
+++ scraper/src/stores/RateLimitingProxyStore.ts
@@ -4,27 +4,45 @@ import { StoreCountry } from '../types/StoreCountry';
 import { IStore } from './IStore';
 
 export class RateLimitingProxyStore implements IStore {
-  private lastRequest = Date.now();
+  private lastScheduledRequest = Date.now();
 
   constructor(private readonly store: IStore, private readonly requestsPerMin: number = 20) {}
 
+  private minRequestTimeDiff = () => 1 / (this.requestsPerMin / 60 / 1000);
+
+  private async delayFor<T>(ms: number, callback: () => Promise<T>): Promise<T> {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(callback()), ms);
+    });
+  }
+
+  private async scheduleRequest<T>(proxiedResource: () => Promise<T>): Promise<T> {
+    const currTimestamp = Date.now();
+    const nextAllowedRequestAt = this.lastScheduledRequest + this.minRequestTimeDiff();
+
+    if (currTimestamp < nextAllowedRequestAt) {
+      const delayRequestFor = nextAllowedRequestAt - currTimestamp;
+      this.lastScheduledRequest = nextAllowedRequestAt;
+      return this.delayFor<T>(delayRequestFor, proxiedResource);
+    }
+
+    this.lastScheduledRequest = currTimestamp;
+    return proxiedResource();
+  }
+
   async getApp(id: string): Promise<App | null> {
-    this.lastRequest = Date.now();
     return this.store.getApp(id);
   }
 
   async getSuggestions(query: string): Promise<string[]> {
-    this.lastRequest = Date.now();
     return this.store.getSuggestions(query);
   }
 
   async getSearchResult(store: StoreCountry, query: string): Promise<string[]> {
-    this.lastRequest = Date.now();
-    return this.store.getSearchResult(store, query);
+    return this.scheduleRequest<string[]>(() => this.store.getSearchResult(store, query));
   }
 
   async getReviews(id: string, store: StoreCountry, sortedBy: ReviewSortCriteria, page: number): Promise<Review[]> {
-    this.lastRequest = Date.now();
     return this.store.getReviews(id, store, sortedBy, page);
   }
 }
